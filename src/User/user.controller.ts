1dd43c0760942938fe5d4ff433f0630229eb1585
sh0723 <pshpark12345@naver.com>
2025-08-01 11:52:32 +0900

    로그인 controller 구현

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,13 +1,14 @@
-import {Body, ConflictException, Controller, Post} from '@nestjs/common'
+import {Body, ConflictException, UnauthorizedException, Controller, Post} from '@nestjs/common'
 import { UserDTO } from './dto/user.dto';
 import { UserService } from './user.service';
+import * as bcrypt from 'bcrypt';
 
 @Controller('user')
 export class userController {
      constructor(private readonly userService: UserService) {}
 
     @Post('/register')
-    async register(@Body() userDTO: UserDTO.register) {
+    async register(@Body() userDTO: UserDTO.Register) {
         
         const userId : string = userDTO.userId;
 
@@ -20,5 +21,24 @@ export class userController {
         const user_entity = await this.userService.rgister(userDTO);
 
         return '회원가입 성공';
+    }
+
+    @Post('/login')
+    async login(@Body() userDTO: UserDTO.LogIn) {
+        const userId : string = userDTO.userId;
+
+        const user = await this.userService.findByUserId(userId);
+
+        if (!user) {
+            throw new UnauthorizedException('아이디를 확인해주세요.');
+        }
+
+        const isMatch : boolean = bcrypt.compareSync(user.password, userDTO.password);
+
+        if (!isMatch) {
+            throw new UnauthorizedException('비밀번호를 확인해주세요.');
+        }
+
+        return '로그인 성공';
     }
 }
